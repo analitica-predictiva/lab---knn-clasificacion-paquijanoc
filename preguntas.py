c4ffc03059917aeeb30f9bf65fb43b13bdc541c9
paquijanoc <111616862+paquijanoc@users.noreply.github.com>
2022-12-02 18:17:16 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -16,7 +16,7 @@ def pregunta_01():
     df = pd.read_csv("house-votes-84.csv", sep=",")
 
     # Cree un vector con la variable de respuesta ('party')
-    y = df["party"].array
+    y = df["party"].copy()
 
     # Extraiga las variables de entrada
     X = df.drop(["party"], axis=1).values
@@ -48,7 +48,7 @@ def pregunta_02():
     df = pd.read_csv("house-votes-84.csv", sep=",")
 
     # Cree un vector con la variable de respuesta ('party')
-    y = df["party"].array
+    y = df["party"].copy()
 
     # Extraiga las variables de entrada
     X = df.drop(["party"], axis=1).values
